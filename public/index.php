7bb67b37153304716c2d9e6d6275f13dfbd34c8e
Robert Searing <robertsearing@hotmail.com>
2011-11-03 19:22:27 -0500

    Made changes - added member, changed enhanced link

--- public/index.php
+++ public/index.php
@@ -19,10 +19,10 @@
 </head>
 
 
-<body style="font-family: sans-serif; margin:0; padding:0; background:url(/images/backg.jpg) center; background-repeat: repeat-y;">
+<body style="font-family: sans-serif; margin:0; padding:0; background:url(Images/backg.jpg) center; background-repeat: repeat-y;">
 <div style="width: 800px; margin-left:auto; margin-right: auto;">
 	<div>
-		<img src="/images/DTNetworkHeader.png" alt="" />
+		<img src="Images/DTNetworkHeader.png" alt="" />
 	</div>
 	<br/>
     <p style="font-size:16px; font-style:italic; width:700px; color:#00F; text-align:center;">
@@ -30,94 +30,94 @@
 		promotes excellence and fun in board gaming.
     </p>
     <br/>
-	<div>
+	<div>	
 		<div style="height:100%;">
-
-			<a href="http://www.dicetower.com/" style="float:left; padding-left:75px;">
-				<img src="/images/dtower.png" alt="" width="150" style="border:none;"/></a>
+		
+			<a href="http://www.dicetower.com/" style="float:left; padding-left:75px;">		
+				<img src="Images/dtower.png" alt="" width="150" style="border:none;"/></a>
 		  	<p style="padding-top:5px; margin-left: 315px;">
-				The flagship podcast of the network, the Dice Tower is an audio show hosted by Eric Summerer and Tom Vasel. Featuring a host of
-				contributors, it covers a wide range of board and card games, with up to the minute news and game reviews. The Dice Tower is
+				The flagship podcast of the network, the Dice Tower is an audio show hosted by Eric Summerer and Tom Vasel. Featuring a host of 
+				contributors, it covers a wide range of board and card games, with up to the minute news and game reviews. The Dice Tower is 
           		also home of the Dice Tower video show - in which Tom Vasel and many others consistently review all manner of board and card games. 			</p>
             <p style="margin-left:315px;">
             	<span style="font-weight:bold;">RSS Feed: </span><a href=	"http://pipes.yahoo.com/pipes/pipe.run?_id=5e24e64f9283370cee91baa60f95bc04&_render=rss"> Audio </a> &nbsp;
-               <a href=	"/http://www.funagain.com/control/viewblog.xml?contentBlogId=TDTE"> Enhanced Audio </a> &nbsp;
+               <a href=	"http://www.funagain.com/control/viewblog.xml?contentBlogId=TDTE> Enhanced Audio </a> &nbsp;
                <a href=	"http://pipes.yahoo.com/pipes/pipe.run?_id=e9eace93b73cbc8e0e7951fbd0f2647b&_render=rss"> Video </a>
             </p>
     </div>
 
 		<div style="height:100%; clear:left; margin-top:15px">
-
-			<a href="http://www.ludology.libsyn.com/" style="float:left; padding-top:22px">
-				<img src="/images/ludology.png" alt="" width="300px" style="border:none;"/></a>
+		
+			<a href="http://www.ludology.libsyn.com/" style="float:left; padding-top:22px">		
+				<img src="Images/ludology.png" alt="" width="300px" style="border:none;"/></a>
 			<p style="margin-left:315px">
-				The Ludology site is about providing an analytical discussion of the how's and why's of the world of board
-				games. Rather than news and reviews, Ludology explores a variety of topics about games from a wider lens, as well as
+				The Ludology site is about providing an analytical discussion of the how's and why's of the world of board 
+				games. Rather than news and reviews, Ludology explores a variety of topics about games from a wider lens, as well as 
 				discuss game history, game design and game players.</p>
 			<p style="margin-left:315px;">
-            	<a href=	"http://ludology.libsyn.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a>
+            	<a href=	"http://ludology.libsyn.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a> 
             </p>
 		</div>
 		<div style="height:100%; margin-top:15px;">
-
-			<a href="http://ryansturm.libsyn.com/" style="float:left; padding-top:65px;">
-				<img src="/images/howtoplay.png" alt="" width="300px" style="border:none;"/></a>
+		
+			<a href="http://ryansturm.libsyn.com/" style="float:left; padding-top:65px;">		
+				<img src="Images/howtoplay.png" alt="" width="300px" style="border:none;"/></a>
 			<p style="padding-top:2px; margin-left:315px;">
-				"The How to Play Podcast" is an audio podcast about teaching andlearning modern board games.  "How to Play" has over
-                twenty-five full explanations of some of the best board games out there.  Each episode contains a description of the game,
-                a full rules explanation and some basic strategy presented by professional educator, Ryan Sturm.  This podcast is intended for
-                use in learning games yourself or learning how to teach great games.  "How to Play" covers entry-level board games all the way
-                up to helping you learn some of the most complex games in our hobby.  "How to Play" also features special episodes
+				"The How to Play Podcast" is an audio podcast about teaching andlearning modern board games.  "How to Play" has over 
+                twenty-five full explanations of some of the best board games out there.  Each episode contains a description of the game, 
+                a full rules explanation and some basic strategy presented by professional educator, Ryan Sturm.  This podcast is intended for 
+                use in learning games yourself or learning how to teach great games.  "How to Play" covers entry-level board games all the way 
+                up to helping you learn some of the most complex games in our hobby.  "How to Play" also features special episodes 
                 including opinion-based episodes, advanced strategy episodes and much more.</p>
 			<p style="margin-left:315px;">
-            	<a href=	"http://ryansturm.libsyn.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a>
+            	<a href=	"http://ryansturm.libsyn.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a> 
             </p>
 		</div>
 		<div style="height:100%; clear:left; margin-top:15px;">
-
-			<a href="http://littlemetaldog.com/" style="float:left; padding-top:45px;">
-				<img src="/images/littlemetal.png" alt="" width="300px" style="border:none;"/></a>
+		
+			<a href="http://littlemetaldog.com/" style="float:left; padding-top:45px;">		
+				<img src="Images/littlemetal.png" alt="" width="300px" style="border:none;"/></a>
 			<p style="margin-left:315px;">
-				The Little Metal Dog Show began back in April 2010 with an idea to produce a podcast on board games that did things a little
-				differently. It took a while to decide the approach that would be taken, but soon an interview-based show quickly became the
-				format to stick with. Each episode normally has a couple of interviews that could really cover any area from the world of
-				gaming – as long as someone is interesting, they're welcome to come on as a guest. From players and designers to mathematicians
-				and film-makers, The Little Metal Dog Show looks far and wide for people who – above all else – have something
-                interesting to say.
+				The Little Metal Dog Show began back in April 2010 with an idea to produce a podcast on board games that did things a little 
+				differently. It took a while to decide the approach that would be taken, but soon an interview-based show quickly became the 
+				format to stick with. Each episode normally has a couple of interviews that could really cover any area from the world of 
+				gaming – as long as someone is interesting, they're welcome to come on as a guest. From players and designers to mathematicians 
+				and film-makers, The Little Metal Dog Show looks far and wide for people who – above all else – have something 
+                interesting to say.				
         	</p>
 			<p style="margin-left:315px;">
-            	<a href=	"http://littlemetaldog.podbean.com/feed/"> <span style="font-weight:bold;">RSS Feed</span> </a>
+            	<a href=	"http://littlemetaldog.podbean.com/feed/"> <span style="font-weight:bold;">RSS Feed</span> </a> 
             </p>
 		</div>
 		<div style="height:100%; margin-top:15px;">
-
-			<a href="http://onboardgames.net/" style="clear:left; float:left;">
-				<img src="/images/onboard.png" alt="" width="300px" style="border:none; padding-top:10px;"/></a>
+		
+			<a href="http://onboardgames.net/" style="clear:left; float:left;">		
+				<img src="Images/onboard.png" alt="" width="300px" style="border:none; padding-top:10px;"/></a>
 			<p style="margin-left:315px;">
-				On Board Games is a show about board and card games.  The guys of On Board Games discuss games from the viewpoints of
-				new and expert players, reviewers, and people from the industry, to allow listeners insight into the gaming hobby
+				On Board Games is a show about board and card games.  The guys of On Board Games discuss games from the viewpoints of 
+				new and expert players, reviewers, and people from the industry, to allow listeners insight into the gaming hobby 
 				so they can enjoy games more thoroughly.</p>
 			<p style="margin-left:315px;">
-            	<a href=	"http://onboardgames.libsyn.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a>
+            	<a href=	"http://onboardgames.libsyn.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a> 
             </p>
 		</div>
 
 		<div style="height:100%; clear:left; margin-top:15px;">
-
-			<a href="http://www.drakkenstrikevideos.com/" style="float:left; padding-top:10px;">
-				<img src="/images/drakkenstrike.png" alt="" width="300px" style="border:none;"/></a>
+		
+			<a href="http://www.drakkenstrikevideos.com/" style="float:left; padding-top:10px;">		
+				<img src="Images/drakkenstrike.png" alt="" width="300px" style="border:none;"/></a>
 			<p style="margin-left:315px;">
-				Drakkenstrike Videos has been providing high-quality board game presentations since we launched our
-				initial Components Breakdown Video Review Series in HD in August of 2010.  With each new video we strive to continue
-				our passion for the industry by providing gamers, designers and publishers from all over the world with a visual
+				Drakkenstrike Videos has been providing high-quality board game presentations since we launched our 
+				initial Components Breakdown Video Review Series in HD in August of 2010.  With each new video we strive to continue 
+				our passion for the industry by providing gamers, designers and publishers from all over the world with a visual 
 				entertainment service unlike any other in our field. </p>
-
+		
 		</div>
 
 		<div style="height:100%; margin-top:15px;">
-
-			<a href="http://www.plaidhatgames.com/" style="clear:left; float:left; margin-left:75px;">
-				<img src="/images/PHGames.png" alt="" width="150px" style="border:none;"/></a>
+		
+			<a href="http://www.plaidhatgames.com/" style="clear:left; float:left; margin-left:75px;">		
+				<img src="Images/PHGames.png" alt="" width="150px" style="border:none;"/></a>
 			<p style="margin-left:315px; padding-top:5px;">
 				The Plaid Hat Podcast takes you inside of one small publisher's adventures
 				in the hobby board game industry. The crew from Plaid Hat Games come
@@ -125,21 +125,32 @@
 				industry, the highs and lows they are experiencing as they publishing
 				games, and much more. </p>
 			<p style="margin-left:315px;">
-            	<a href=	"http://www.plaidhatgames.com/podcasts/PlaidHat_podcast.xml"> <span style="font-weight:bold;">RSS Feed</span> </a>
+            	<a href=	"http://www.plaidhatgames.com/podcasts/PlaidHat_podcast.xml"> <span style="font-weight:bold;">RSS Feed</span> </a> 
             </p>
 		</div>
-
+	
 		<div style="height:100%; clear:left; margin-top:15px;">
-
-			<a href="http://njagp.libsyn.com/" style="clear:left; float:left; margin-left:75px;">
-				<img src="/images/NJAGP.png" alt="" width="150px" style="border:none;"/></a>
+		
+			<a href="http://njagp.libsyn.com/" style="clear:left; float:left; margin-left:75px;">		
+				<img src="Images/NJAGP.png" alt="" width="150px" style="border:none;"/></a>
 			<p style="margin-left:315px; padding-top:5px;">
-				NJAGP is hosted by Jim Reed, Jeff Scott, and Matthew McHale. The focus on gaming spans the realm,
-				from board and card games to role playing games and touches on everything in between including miniature
-				gaming and war gaming. This podcast also takes a look "out of the box", at gaming and pop culture in movies,
+				NJAGP is hosted by Jim Reed, Jeff Scott, and Matthew McHale. The focus on gaming spans the realm, 
+				from board and card games to role playing games and touches on everything in between including miniature 
+				gaming and war gaming. This podcast also takes a look "out of the box", at gaming and pop culture in movies, 
 				television, and comics. NJAGPs focus is not only to be informative but to be entertaining and fun! </p>
 			<p style="margin-left:315px;">
-            	<a href=	"http://njagp.libsyn.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a>
+            	<a href=	"http://njagp.libsyn.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a> 
+            </p>
+		</div>
+        
+        <div style="height:100%; clear:left; margin-top:15px;">
+		
+			<a href="http://www.shutupshow.com/" style="clear:left; float:left; padding-top:35px;">		
+				<img src="Images/SUSDBanner.png" alt="" width="300px" style="border:none;"/></a>
+			<p style="margin-left:315px; padding-top:5px;">
+				A show all about board games, card games, and all the other games you can play in your very own house, Shut Up & Sit Down is made 																		by two men who never quite grew up. Coming from London, England, each half-hour episode includes game reviews and skits, as well as more than a little bickering. We believe gaming is one of the most exciting hobbies anyone can enjoy and we want to show you why we're so passionate about it. Why not tune in?</p>
+			<p style="margin-left:315px;">
+            	<a href=	"http://www.shutupshow.com/rss"> <span style="font-weight:bold;">RSS Feed</span> </a> 
             </p>
 		</div>
 	</div>
