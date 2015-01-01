64c233e91c2cf9fa6ad57035cf68edcf9660f620
Robert Searing <robertsearing@hotmail.com>
2015-01-01 17:33:56 -0600

    Removed Advertising Banner for January
    Modified index.php file simply commented banner to hide.

--- public/index.php
+++ public/index.php
@@ -33,9 +33,9 @@
 	
 	          
 	
-	<a  href="https://www.kickstarter.com/projects/funforge/zna" target="_blank">
+<!--	<a  href="https://www.kickstarter.com/projects/funforge/zna" target="_blank">
         <img id="adbanner" src="/images/ZNA_LIVE_728x90.png" alt="Check Out Hollywood - New From Hobby World Games!!" title="Click To Go To The Hollywood Kickstarter Page" />
-	</a>
+	</a> -->
     <br/>
 	
 	
